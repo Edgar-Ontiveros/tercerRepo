8883ca9f9cc6aad993c9634b492d3e93262083ae
Edgar Ontiveros <eontiveros@herinox.com.mx>
2026-05-15 17:26:08 -0600

    Primer Lanzamiento

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="tercerRepo",                              # Nombre del paquete
+    version="0.1.0",                                # Versión inicial
+    packages=find_packages(),                       # Paquetes a incluir
+    description="Un paquete pip simple de saludo",  # Breve descripción
+    author="Edgar Ontiveros",                         # Tu nombre
+    author_email="edgarontiverosfm@gmail.com",                 # Tu correo electrónico
+    url="https://github.com/Edgar-Ontiveros/tercerRepo",     # URL del proyecto
+)
